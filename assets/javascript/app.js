05153a41c3c1a80fa40ef20118471178a89ffe01
P Reijerkerk <preijerkerk@gmail.com>
2018-08-28 21:02:47 -0400

    changed to the full JQUERY as opposed to the slim; takes the clicked show and performs an ajax call to the giphy api; creates a variable for the results then iterates down the length of the results created a div with a GIF rating and sets the data attributes for still and animate on the gif, then appends to the html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var shows = ["futurama","south park","archer","rick and morty","hell's kitchen","masterchef"];
+    var shows = ["futurama","south park","archer","rick and morty"];
 
 //adds button for the original shows array
 function renderButtons() {
@@ -18,6 +18,44 @@ $("#add-show").on("click", function() {
     shows.push(show);
     renderButtons();
     return;
+});
+
+//ajax call to the giphy api
+$("button").on("click", function () {
+    var show = $(this).attr("data-show");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=xxKKQ5ohrA8XTNvIi8DzYQhKeulRg0Uh&limit=10"
+//Taking the queryURL, gets a response from the server
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response) {
+       //sets a variable results equal to the data from the server response
+        var results = response.data;
+        //empty the shows-gif div on clicking a new button
+        $("#shows-gif").empty();
+        //iterates along the ten gifs grabbed from the response.data
+        for(var i=0; i < results.length; i++) {
+            var showDiv = $("<div>");
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            var showImage = $("<img>");
+            //sets the attr of showImage for a source of the still image url (default image)
+            showImage.attr("src", results[i].images.original_still.url);
+            //sets the attr data-still to the images still url 
+            showImage.attr("data-still", results[i].images.original_still.url);
+            //sets the attr for data-animate to the animated gif
+            showImage.attr("data-animate", results[i].images.original.url);
+            //sets the default data-state to still (default state)
+            showImage.attr("data-state", "still");
+            //sets class of the image to gif
+            showImage.attr("class", "gif");
+            //appends the paragraph for the gifs rating to the showDiv
+            showDiv.append(p);
+            //appends the showImage (default state is a still gif) to the showDiv
+            showDiv.append(showImage);
+            //appends the showDiv containing the gif rating and gif image to the html div
+            $("#shows-gif").append(showDiv);
+        }
+    });
 });
 
 
